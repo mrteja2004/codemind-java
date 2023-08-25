63841c7dac7fa27122a146b07032d4aff4888d87
TEJA GANDHAM 05 <119840454+mrteja2004@users.noreply.github.com>
2023-08-25 11:48:39 +0530

    import java.util.*; public class Test{     public static void main(String[] args){         Scanner op = new Scanner(System.in);         int a= op.nextInt();         int b= a*(a-1)/2;         System.out.print(b);   } }

---
+++ Program_to_find_the_maximum_number_of_handshakes_.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Test{
+    public static void main(String[] args){
+        Scanner op = new Scanner(System.in);
+        int a= op.nextInt();
+        int b= a*(a-1)/2;
+        System.out.print(b);
+  }
+}

47386dd0e8272c408e4fabc9b2521372b954342a
TEJA GANDHAM 05 <119840454+mrteja2004@users.noreply.github.com>
2023-08-25 12:17:14 +0530

    import java.util.*; public class Test{     public static void main(String[] args){         Scanner op = new Scanner(System.in);         String a = op.next();         int b=Integer.parseInt(a,8);         System.out.print(b);
        }
    }

---
+++ Octal_to_Decimal_Conversion.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Test{
+    public static void main(String[] args){
+        Scanner op = new Scanner(System.in);
+        String a = op.next();
+        int b=Integer.parseInt(a,8);
+        System.out.print(b);
+        
+    }
+}

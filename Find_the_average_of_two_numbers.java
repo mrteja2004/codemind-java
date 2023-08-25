952105dda7abc45d9e24959e68faf044161d6d4a
TEJA GANDHAM 05 <119840454+mrteja2004@users.noreply.github.com>
2023-08-25 11:18:42 +0530

    import java.util.*; public class Siddu{     public static void main(String[] args){         Scanner op=new Scanner(System.in);         double a=op.nextDouble();         double b=op.nextDouble();         double c = (a+b)/2;         System.out.printf("%.4f",c);     } }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Siddu{
+    public static void main(String[] args){
+        Scanner op=new Scanner(System.in);
+        double a=op.nextDouble();
+        double b=op.nextDouble();
+        double c = (a+b)/2;
+        System.out.printf("%.4f",c);
+    }
+}

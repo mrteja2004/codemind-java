dd7a0da930fe248cf0cfe6f919c9c40334711ff1
TEJA GANDHAM 05 <119840454+mrteja2004@users.noreply.github.com>
2023-08-25 11:37:18 +0530

    import java.util.*; public class Test{     public static void main(String[] args){         Scanner op = new Scanner(System.in);         String a = op.nextLine();         System.out.print(a);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Test{
+    public static void main(String[] args){
+        Scanner op = new Scanner(System.in);
+        String a = op.nextLine();
+        System.out.print(a);
+    }
+}

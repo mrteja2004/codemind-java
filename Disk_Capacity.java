9c832301667bea8d6382a500fca03f56ee132250
TEJA GANDHAM 05 <119840454+mrteja2004@users.noreply.github.com>
2023-08-25 12:00:02 +0530

    import java.util.*; public class Test{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         int a= sc.nextInt();         int b= sc.nextInt();         int c= sc.nextInt();         int d= a*2*b*c*512;         System.out.print(d);     } }

---
+++ Disk_Capacity.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Test{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a= sc.nextInt();
+        int b= sc.nextInt();
+        int c= sc.nextInt();
+        int d= a*2*b*c*512;
+        System.out.print(d);
+    }
+}

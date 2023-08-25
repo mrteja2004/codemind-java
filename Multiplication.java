d7066e6d622210010541dd6ef7043de6ff54599a
TEJA GANDHAM 05 <119840454+mrteja2004@users.noreply.github.com>
2023-08-25 11:23:35 +0530

    import java.util.*; public class Uday{     public static void main(String[] args){         Scanner op= new Scanner(System.in);         int a= op.nextInt();         int b= op.nextInt();         int c = a*b;         System.out.println(c);     } }

---
+++ Multiplication.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Uday{
+    public static void main(String[] args){
+        Scanner op= new Scanner(System.in);
+        int a= op.nextInt();
+        int b= op.nextInt();
+        int c = a*b;
+        System.out.println(c);
+    }
+}

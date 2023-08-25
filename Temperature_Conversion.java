b3455c830e349e9765f5297c3155666caa664819
TEJA GANDHAM 05 <119840454+mrteja2004@users.noreply.github.com>
2023-08-25 12:08:53 +0530

    import java.util.*; public class Test{     public static void main(String[] args){         Scanner op = new Scanner(System.in);         int a = op.nextInt();         double b = (32+(a*1.8));         System.out.printf("%.2f", b);     } }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Test{
+    public static void main(String[] args){
+        Scanner op = new Scanner(System.in);
+        int a = op.nextInt();
+        double b = (32+(a*1.8));
+        System.out.printf("%.2f", b);
+    }
+}

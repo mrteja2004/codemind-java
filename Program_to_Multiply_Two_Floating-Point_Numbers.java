1dd3e1db0a985b074f00346c123ac41fa4e7fe75
TEJA GANDHAM 05 <119840454+mrteja2004@users.noreply.github.com>
2023-08-25 14:33:43 +0530

    import java.util.*; public class Test{     public static void main(String[] args){         Scanner op= new Scanner(System.in);         float a=op.nextFloat();         float b=op.nextFloat();         float c=a*b;         System.out.printf("%.2f", c);     } }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Test{
+    public static void main(String[] args){
+        Scanner op= new Scanner(System.in);
+        float a=op.nextFloat();
+        float b=op.nextFloat();
+        float c=a*b;
+        System.out.printf("%.2f", c);
+    }
+}

33c683284649cf990c33838d480e16a5bfaf4f6d
TEJA GANDHAM 05 <119840454+mrteja2004@users.noreply.github.com>
2023-08-29 10:55:03 +0530

    import java.util.*; public class Test{     public static void main(String[] ants){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         if(n%3==0)         System.out.print("Pling");          if(n%5==0)         System.out.print("Plang");          if(n%7==0)         System.out.print("Plong");          if(n%3!=0 && n%5!=0 && n%7!=0)         System.out.print(Integer.toString(n));     } }

---
+++ RainDrop.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Test{
+    public static void main(String[] ants){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n%3==0)
+        System.out.print("Pling");
+         if(n%5==0)
+        System.out.print("Plang");
+         if(n%7==0)
+        System.out.print("Plong");
+         if(n%3!=0 && n%5!=0 && n%7!=0)
+        System.out.print(Integer.toString(n));
+    }
+}
